2e7340ef0c3cf73981da99aa881e9791f9dd9ee2
VempatiSiva456 <102824586+VempatiSiva456@users.noreply.github.com>
2023-03-03 12:07:20 +0530

    Final

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+const path = require('path');
 const cors = require('cors')
 // dotenv.config({ path: "./config.env" });
 
